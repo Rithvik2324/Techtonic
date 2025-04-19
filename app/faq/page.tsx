0c1fecfe3aff96bc38d1b3e585bed283d56ea9c0
Rithvik2324 <kamireddyrithvik@gmail.com>
2025-04-19 14:49:56 +0530

    start

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,11 +1,9 @@
 import Faq from "@/components/faq"
 
-//import { ClientWrapper } from "@/components/client-wrapper"
 
 export default function FaqPage() {
   return (
-    //<ClientWrapper>
       <Faq />
-    //</ClientWrapper>
+
   )
 }
